67ce6441ffc4cae03bf0788da9c79bde7a8823c9
git <gitlab@privatenebula.com>
2021-05-01 16:29:57 +0000

    I made it work. Sorry if I overwrite your stuff, but were just pushing to main which is not recommended anyway.

--- chatServer.c
+++ chatServer.c
@@ -29,8 +29,11 @@ void do_error(const char *text, int enr);
 //Inserts a user - assumes that length includes the null pointer in its calculation
 void insert(int fd, char *username, int length);
 struct userConnection* userRemove(int fd);
+struct userConnection* getUser(int fd);
+int sendToAll(char *message, int len);
 int findUserFD(int fd);
-int findUserName(char name[], int len);
+int findUserName(char *name, int len);
+void printList();
 //Functions:
 //Sends to all the sockets
 
@@ -41,7 +44,7 @@ int findUserName(char name[], int len);
 
 struct userConnection {
 	int fd;
-	char **username;
+	char *username;
 	int length;
 	struct userConnection* next;
 };
@@ -67,13 +70,16 @@ int main(int argc, char **argv)
    int nrrec = 0;
    fd_set set, rset;
    char *taken = "1";
-
+   char *collon = ": ";
+   char message[MAXLINE+1];
    int found = 0; //Was the username found or not
    struct userConnection *currentConn = NULL;
-
-
+   short mesLen = 0;
+   int unameLen=0;
    ssock = socket(AF_INET, SOCK_STREAM, 0);
-
+   const int       optVal = 1;
+   const socklen_t optLen = sizeof(optVal);
+   setsockopt(ssock, SOL_SOCKET, SO_REUSEADDR, (void*) &optVal, optLen);
    if (ssock < 0)
       do_error("socket() error", errno);
 
@@ -113,7 +119,7 @@ int main(int argc, char **argv)
    {
       // copy main set to receive set
       rset = set;
-
+      //printList();
       // block indefinitely until someone is ready
       res = select(FD_SETSIZE, &rset, NULL, NULL, NULL);
 
@@ -126,7 +132,8 @@ int main(int argc, char **argv)
          {
             if(i == ssock) // server is ready to accept
             {
-               bzero(&caddr, sizeof(caddr));
+              printf("Client connection attempt\n");
+              bzero(&caddr, sizeof(caddr));
                csock = accept(ssock, (struct sockaddr *) &caddr, &csize);
                if (csock == -1)
                   do_error("accept() error", errno);
@@ -135,6 +142,7 @@ int main(int argc, char **argv)
                   // add new client socket to master set
                   FD_SET(csock, &set);
 				  //Add to userConnection
+          printf("New Client Connected %d",csock);
 
                }
             }
@@ -149,31 +157,43 @@ int main(int argc, char **argv)
                }
                else if (res > 0)
                {
-				    printf("Received: %s\n", recline);
-					recline[res] = 0;
+                 recline[res] = 0;
+				    printf("Received: %d %s\n",res, &(recline[3]));
+					//recline[res] = 0;
+          //printList();
 					//Switch statement for operations
 					switch(recline[0]){
 						case JOIN:
 							printf("Joining.\n");
               //Extract User Name
-              int unameLen = res-2;
+              unameLen = res-2;
+              //`printf("Before\n");
 
-              							//1. iterate through list of usernames and find the filedescriptor
+              //printf("After");
+							//1. iterate through list of usernames and find the filedescriptor
 							found = findUserName(&(recline[3]),unameLen);
+
+              printf("User found is %d\n",found);
 							if (found<0){
 								//2.0 Set username & length in userConnection
-               found = findUserFD(i);
-              if (!found) {
-                insert(i,&(recline[3]),unameLen);
-              }
-              else {
-                //Be mean and do not let them change the name.
-              }
+                found = findUserFD(i);
+                printf("User file descriptor is: %d\n",found);
+                if (found<=0) {
+                  insert(i,&(recline[3]),unameLen);
+                  send(i,&(recline[0]),1,0);
+                }
+                else {
+                  //Be mean and do not let them change the name.
+                  //printf("DBG i: %d found: %d",i,found);
+                  recline[0]+=1;
+                  send(i,&(recline[0]),1,0);
+                }
 								//insert(i, &recline[3], );
 							}
 							//2.1 or if username is used, send used to client
 							else {
-								send(ssock, taken, 1, 0);
+                recline[0]+=1;
+								send(i, &(recline[0]), 1, 0);
 							}
 							break;
 						case LEAVE:
@@ -187,9 +207,32 @@ int main(int argc, char **argv)
               currentConn = NULL;
 							break;
 						case TALK:
-							//1. check to see if username is set/get username
+							//1. check to see if username is set/get usernamea
+              currentConn = getUser(i);
+              if (currentConn==NULL) {
+                send(i,&(recline[0]),1,0);
+                printf("Not on server");
+                break;
+              }
+              //heawder 3 username is len message is res
+              mesLen = currentConn->length+res-1;
+              //printf("mess len %x",mesLen);
+              message[0] = TALK;
+              message[2] = (char)(mesLen&0x00FF);
+              message[1] = (char)(mesLen>>8);
+              //printf("low bye %x high bye %x",message[1],message[2]);
 							//2. get the message (in recline)
+              strcpy(&(message[3]),currentConn->username);
+              // printf(message);
+
+              strcpy(&(message[currentConn->length+2]),collon);
+              // printf(message);
+
+              strcpy(&(message[currentConn->length+4]),&(recline[3]));
+              //printf(&(message[3]));
 							//3. concatonate username first (Username: message)
+              //Length currentConn->length+2+res-1
+              sendToAll(message,mesLen+3);
 							//4. call method to send to everyone
 							break;
 						case LIST:
@@ -211,6 +254,7 @@ int main(int argc, char **argv)
 
 					 }*/
 	               }
+          //printList();
 	            }
 	         }
 	      }
@@ -220,17 +264,25 @@ int main(int argc, char **argv)
    return 0;
 }
 //Return -1 if username not found else return the fd of the owner.
-int findUserName(char name[],int len) {
+int findUserName(char *name,int len) {
+  //printf("Finding user name\n");
   struct userConnection *cur = firstConnection;
   if (firstConnection == NULL) {
+    //printf("List Empty\n");
     return -1;
   }
+  //printf("Starting Loop\n");
   while (cur!=NULL) {
-    if (cur->length!=len) continue;
-    if (0==strcmp(*(cur->username),name)) {
+
+    if (cur->length == len) {
+    //printf("Comparing: %s and %s\n",cur->username,name);
+    if (0==strcmp(cur->username,name)) {
       return cur->fd;
     }
   }
+  cur=cur->next;
+  }
+  //printf("Not found\n");
   return -1;
 }
 
@@ -255,7 +307,18 @@ int findUserFD(int fd){
 	//Found the user if we got here
 	return 1;
 }
-
+int sendToAll(char *message, int len) {
+  struct userConnection *cur = firstConnection;
+  if (firstConnection == NULL) {
+    return -1;
+  }
+  while (cur!=NULL) {
+    send(cur->fd,message,len,0);
+    printf("Sent %s to %d\n",&(message[3]),cur->fd);
+    cur = cur->next;
+  }
+  return 1;
+}
 //Inserts a user - assumes that length includes the null pointer in its calculation
 void insert(int fd, char *username, int length) {
    struct userConnection *node = (struct userConnection*) malloc(sizeof(struct userConnection));
@@ -266,9 +329,43 @@ void insert(int fd, char *username, int length) {
    node->length = length;
    node->next = firstConnection;
    firstConnection = node;
-   strcpy(*(node->username),username);
+   strcpy(node->username,username);
 
 }
+void printList() {
+  printf("List: ");
+struct userConnection *cur = firstConnection;
+  if(firstConnection==NULL) {
+    printf("NULL\n");
+    return;
+  }
+while(cur!=NULL) {
+
+			//prev = cur;
+      printf("%d, ",cur->fd);
+			cur = cur->next;
+
+	}
+
+}
+struct userConnection *getUser(int fd) {
+   struct userConnection *cur = firstConnection;
+   //struct userConnection *prev = NULL;
+
+   if(firstConnection == NULL) {
+      return NULL;
+   }
+   while(cur->fd != fd) {
+      if(cur->next == NULL) {
+         return NULL;
+      } else {
+         //prev = cur;
+         cur = cur->next;
+      }
+   }
+   return cur;
+}
+
 
 //Removes a user from the list of users
 struct userConnection *userRemove(int fd) {
