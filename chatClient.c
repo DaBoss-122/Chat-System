85aee447111ec0344945c6dc4a3f176201c8177e
DaBoss-122 <70650044+DaBoss-122@users.noreply.github.com>
2021-05-01 22:07:01 -0500

    Proper indentation to client

--- chatClient.c
+++ chatClient.c
@@ -9,7 +9,7 @@
 #include <string.h>        // for strerror
 #include <errno.h>         // for errno
 #include <fcntl.h>         // for fcntl
-#include <string.h>       // for strcmp
+#include <string.h>        // for strcmp
 
 #define ECHO_PORT 9000
 #define MAXLINE 4096
@@ -23,53 +23,54 @@
 extern int errno;
 void do_error(const char *text, int enr);
 int main(int argc, char **argv) {
-    int servsockfd, sockfd, numbers, clientsocklen;
-    int res,flags;
-    fd_set set, rset;
-    char message[MAXLINE+1];
-    char recline[MAXLINE+1];
-    char *username;
-    int ipaddr;
-    short port=9000;
-    struct sockaddr_in servaddr, clientaddr;
-    int amountReceived;
-    sockfd = socket(AF_INET, SOCK_STREAM,0);
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
+   int servsockfd, sockfd, numbers, clientsocklen;
+   int res,flags;
+   fd_set set, rset;
+   char message[MAXLINE+1];
+   char recline[MAXLINE+1];
+   char *username;
+   int ipaddr;
+   short port=9000;
+   struct sockaddr_in servaddr, clientaddr;
+   int amountReceived;
+   sockfd = socket(AF_INET, SOCK_STREAM,0);
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_port = htons(port);
 
-    if (argc<3) {
-        printf("Not enough arguments. Include the ip name.\n");
-        return 0;
-    }
-    else {
-        inet_pton(AF_INET,argv[1],&(servaddr.sin_addr));
-        //printf("%d\n",port);
-        username = argv[2];
-    }
-    int userLen=strlen(username);
+   if (argc<3) {
+      printf("Not enough arguments. Include the ip name.\n");
+      return 0;
+   }
+   else {
+      inet_pton(AF_INET,argv[1],&(servaddr.sin_addr));
+      username = argv[2];
+   }
+   
+   int userLen=strlen(username);
    connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
 
-      message[0] = JOIN;
-      message[1] = (userLen>>8)&0x00FF;
-      message[2] = userLen&0x00FF;
-      strcpy(&(message[3]),username);
-      send(sockfd,message,userLen+3,0);
-      res = recv(sockfd,recline,MAXLINE,0);
-      if (res<=0) {
-        printf("Received not a response.\n");
-      } else {
-        if (recline[0]==0) {
-          printf("Name Accepted\n");
-          //break;
-        }
-        else {
-          printf("User name already taken.\n");
-          return 0;
-        }
+   message[0] = JOIN;
+   message[1] = (userLen>>8)&0x00FF;
+   message[2] = userLen&0x00FF;
+   strcpy(&(message[3]),username);
+   send(sockfd,message,userLen+3,0);
+   res = recv(sockfd,recline,MAXLINE,0);
+   
+   if (res<=0) {
+      printf("Received not a response.\n");
+   } 
+   else {
+      if (recline[0]==0) {
+         printf("Name Accepted\n");
+      }
+      else {
+         printf("User name already taken.\n");
+         return 0;
+      }
    }
    printf("%s>\n",username);
-  flags = fcntl(sockfd, F_GETFL, 0);
+   flags = fcntl(sockfd, F_GETFL, 0);
 
    if (flags == -1)
       do_error("fcntl(F_GETFD) error", errno);
@@ -93,40 +94,39 @@ int main(int argc, char **argv) {
    if(res == -1)
       do_error("fcntl(F_SETFD) error", errno);
 
-
    // zero select sets for select
    FD_ZERO(&set);
    FD_ZERO(&rset);
-  // infinite loop to process activity
+
+   // infinite loop to process activity
    for(;;)
    {
-//Trying to receive non-blocking another way. Ignore this mess.
-     if (( res = read(0, recline, MAXLINE))>0) {
-//printf("Got it\n");
-       if (strcmp(recline,"@exit")==0) {
-          message[0] = LEAVE;
-          send(sockfd,message,1,0);
-      } else {
-          message[0] = TALK;
-          message[1] = (res>>8)&0x00FF;
-          message[2] = res&0x00FF;
-          strcpy(&(message[3]),recline);
-          send(sockfd,message,res+3,0);
-          printf("%s>",username);
+      //Trying to receive non-blocking another way. Ignore this mess.
+      if (( res = read(0, recline, MAXLINE))>0) {
+         if (strcmp(recline,"@exit")==0) {
+            message[0] = LEAVE;
+            send(sockfd,message,1,0);
+         }
+         else {
+         message[0] = TALK;
+         message[1] = (res>>8)&0x00FF;
+         message[2] = res&0x00FF;
+         strcpy(&(message[3]),recline);
+         send(sockfd,message,res+3,0);
+         printf("%s>",username);
+         }
       }
-     }
-     res = recv(sockfd, recline, MAXLINE, 0);
-     if (res == -1)
+      res = recv(sockfd, recline, MAXLINE, 0);
+      if (res == -1)
       {
-       continue;
+         continue;
       }
-     else if (res > 0)
-     {
-      recline[res] = 0;
-      printf("%s",&(recline[3]));
-     }
-  }
-   //close(sockfd);
+      else if (res > 0)
+      {
+         recline[res] = 0;
+         printf("%s",&(recline[3]));
+      }
+   }
    printf("Program Terminated Successfully.\n");
    return 0;
 
